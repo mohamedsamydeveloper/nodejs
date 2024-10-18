9e3decc7e2952a3c63fae9f41efe073d166d75c8
mohamedsamydeveloper <167178999+mohamedsamydeveloper@users.noreply.github.com>
2024-10-18 17:10:21 +0300

    Update server.js

--- nodejs/api/server.js
+++ nodejs/api/server.js
@@ -1,13 +1,3 @@
-const http = require('http');
-const PORT = process.env.PORT || 3000;
-
-const requestListener = (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello\n');
-};
-
-const server = http.createServer(requestListener);
-
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default (req, res) => {
+    res.status(200).json({ message: "Hello from Node.js!" });
+  };
